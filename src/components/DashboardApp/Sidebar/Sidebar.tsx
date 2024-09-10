05cf832a9192ff29d4f9909618365fb3be94046c
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-09-10 04:17:10 +0000

    update dashboard sidebar

--- src/components/DashboardApp/Sidebar/Sidebar.tsx
+++ src/components/DashboardApp/Sidebar/Sidebar.tsx
@@ -48,7 +48,8 @@ export function DashboardSidebar() {
     <Sidebar open={open} setOpen={setOpen} animate={false}>
       <SidebarBody className="justify-between gap-10">
         <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-          {open ? <Logo /> : <LogoIcon />}
+          {/* {open && ? <Logo /> : <LogoIcon />} */}
+          <Logo />
           <div className="mt-8 flex flex-col gap-2">
             {links.map((link, idx) => (
               <SidebarLink key={idx} link={link} />
